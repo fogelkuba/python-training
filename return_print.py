307c98951cc3635b9af05330e24e38686a84e594
fogelkuba <fogelkuba@gmail.com>
2018-11-21 19:53:07 +0100

    fix

--- return_print.py
+++ return_print.py
@@ -1,12 +1,19 @@
 def returning():
     return 10
 
-// default value
+# default value
 def printing(x = 69):
     print(x)
 
+#
+# returning()
+#
+# printing(10)
+# printing()
 
-returning()
 
-printing(10)
-printing()
+def convert(c):
+    f = c * 9/5 + 32
+    print(f)
+
+convert(123)
