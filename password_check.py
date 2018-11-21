14cd917cfe9111467c6dcb28017ffc5f53adce95
fogelkuba <fogelkuba@gmail.com>
2018-11-21 22:27:17 +0100

    fix

--- password_check.py
+++ password_check.py
@@ -2,4 +2,4 @@ correct_pass = 'python123'
 password = input("Enter password: ")
 
 while correct_pass != password:
-    password =  input('Error, try again: ')
+    password = input('Error, try again: ')
