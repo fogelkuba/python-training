c955464e3d2022917f4cdc6f04e1faaf37412d32
fogelkuba <fogelkuba@gmail.com>
2018-11-21 22:48:35 +0100

    fix

--- password_check.py
+++ password_check.py
@@ -1,8 +1,10 @@
 correct_pass = 'python123'
 name = input('Your name: ')
+surname = input('Your surname: ')
 password = input("Enter password: ")
 
 while correct_pass != password:
     password = input('Error, try again: ')
 
-print('Hi %s, you are logged in' % name)
+# print('Hi %s, you are logged in' % name)
+print('Hi %s %s, you are logged in' % (name, surname))
