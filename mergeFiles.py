33d0ed035d004ca2c90c16383cbe40b112c855f1
fogelkuba <fogelkuba@gmail.com>
2018-11-25 12:56:57 +0100

    fix

--- mergeFiles.py
+++ mergeFiles.py
@@ -1,2 +1,8 @@
-import datetime as datetime
+import datetime, os
+print(str(datetime.datetime.now().strftime("%y-%m-%d-%H-%M")))
 
+
+def mergeFiles(path = './filesDir'):
+
+    for filename in os.listdir(path):
+        print(filename)
