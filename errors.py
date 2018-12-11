c1785e9d572c074df4951a83bef160f53ba762ad
fogelkuba <fogelkuba@gmail.com>
2018-12-11 21:31:02 +0100

    fix

--- errors.py
+++ errors.py
@@ -2,8 +2,8 @@ def divide(a, b):
     try:
         return a / b
     except:
-        print('Nie ma takiego dzielenia !1')
-        return False
+        return 'Nie ma takiego dzielenia !1'
 
 
 print(divide(1,0))
+print('end')
