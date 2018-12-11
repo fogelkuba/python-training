904e3b96066c844c95e13aab1832c1c475615cda
fogelkuba <fogelkuba@gmail.com>
2018-12-11 23:09:42 +0100

    fix

--- dictionary/app.py
+++ dictionary/app.py
@@ -1,16 +1,15 @@
 import json
-import difflib
 from difflib import SequenceMatcher, get_close_matches
 
 data = json.load(open("./data.json"))
 
 def translate(w):
     w = w.lower()
-
     get_close_matches(w, data.keys(), n = 3)
-
     if w in data:
         return data[w]
+    elif len( get_close_matches(w, data.keys()) ) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
     else:
         return "The word doesn' exist. Please double check."
 
