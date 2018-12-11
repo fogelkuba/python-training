1a7d9dc939c5904114e583df5fccfe3538ac7596
fogelkuba <fogelkuba@gmail.com>
2018-12-11 22:40:24 +0100

    fix

--- dictionary/app.py
+++ dictionary/app.py
@@ -1,9 +1,15 @@
 import json
+import difflib
+from difflib import SequenceMatcher
 
 data = json.load(open("./data.json"))
 
-def translate(word):
-    return data[word]
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "The word doesn' exist. Please double check."
 
 word = input("Enter word: ")
 
