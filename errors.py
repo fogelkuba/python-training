88fed9c31a90eb7476353e7499802824e2919887
fogelkuba <fogelkuba@gmail.com>
2018-12-11 21:34:01 +0100

    fix

--- errors.py
+++ errors.py
@@ -3,7 +3,10 @@ def divide(a, b):
         return a / b
     except ZeroDivisionError:
         return 'Nie ma takiego dzielenia !1'
+    # except TypeError:
+    #     return 'Podaj argument'
 
 
 print(divide(1,0))
+print(divide(1))
 # print('end')
