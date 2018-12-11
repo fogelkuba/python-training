f7465660fa4f04ffbf556eac512f54b29cca2f25
fogelkuba <fogelkuba@gmail.com>
2018-12-11 23:40:47 +0100

    fix

--- dictionary/app.py
+++ dictionary/app.py
@@ -9,11 +9,15 @@ def translate(w):
     if w in data:
         return data[w]
     elif len( get_close_matches(w, data.keys()) ) > 0:
-        confirm = input("Did you mean %s instead? Enter Y if yes, N if no." % get_close_matches(w, data.keys())[0])
-        if confirm == "y":
+        confirm = input("Did you mean %s instead? Enter Y if yes, N if no:" % get_close_matches(w, data.keys())[0])
+        if confirm.lower() == "y":
             return data[get_close_matches(w, data.keys())[0]]
+        elif confirm.lower() == "n":
+            return "The word doesn't exist. Please double check."
+        else:
+            return "We didn't understand your query."
     else:
-        return "The word doesn' exist. Please double check."
+        return "The word doesn't exist. Please double check."
 
 word = input("Enter word: ")
 
