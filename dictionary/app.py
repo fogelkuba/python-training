4367cb16f47acb2502a246b7deacc49d378813bf
fogelkuba <fogelkuba@gmail.com>
2018-12-11 22:59:56 +0100

    fix

--- dictionary/app.py
+++ dictionary/app.py
@@ -1,11 +1,14 @@
 import json
 import difflib
-from difflib import SequenceMatcher
+from difflib import SequenceMatcher, get_close_matches
 
 data = json.load(open("./data.json"))
 
 def translate(w):
     w = w.lower()
+
+    get_close_matches(w, data.keys(), n = 3)
+
     if w in data:
         return data[w]
     else:
