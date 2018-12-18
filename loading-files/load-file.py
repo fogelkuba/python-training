892c4b4edc67abee520d1e714c9974d07e58a0c7
fogelkuba <fogelkuba@gmail.com>
2018-12-18 20:17:01 +0100

    fix

---
+++ loading-files/load-file.py
@@ -0,0 +1,20 @@
+import pandas
+
+csv = pandas.read_csv('./supermarkets.csv')
+print()
+print(csv)
+
+print('================================')
+
+txt = pandas.read_csv('./supermarkets-commas.txt')
+print(txt)
+
+print('================================')
+
+json = pandas.read_json('./supermarkets.json')
+print(json)
+
+print('================================')
+
+xls = pandas.read_excel('./supermarkets.xlsx')
+print(xls)
