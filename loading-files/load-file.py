abb5a877d5847da38257c40303f2fc1227b2287c
fogelkuba <fogelkuba@gmail.com>
2018-12-18 20:27:38 +0100

    fix

--- loading-files/load-file.py
+++ loading-files/load-file.py
@@ -1,20 +1,20 @@
 import pandas
 
-csv = pandas.read_csv('./supermarkets.csv')
-print()
+csv = pandas.read_csv('./supermarkets.csv', header=None)
+print('CSV FILE: ')
 print(csv)
 
-print('================================')
-
-txt = pandas.read_csv('./supermarkets-commas.txt')
-print(txt)
-
-print('================================')
-
-json = pandas.read_json('./supermarkets.json')
-print(json)
-
-print('================================')
-
-xls = pandas.read_excel('./supermarkets.xlsx')
-print(xls)
+# print('================================')
+#
+# txt = pandas.read_csv('./supermarkets-commas.txt')
+# print(txt)
+#
+# print('================================')
+#
+# json = pandas.read_json('./supermarkets.json')
+# print(json)
+#
+# print('================================')
+#
+# xls = pandas.read_excel('./supermarkets.xlsx')
+# print(xls)
